cc97a93d27dbcf658710a822c52cdfbfdf2012ee
mbalzano19 <125160521+mbalzano19@users.noreply.github.com>
2023-05-29 12:38:11 -0400

    finished mvp

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ print("Let's play Py-Pac-Poe!")
 print('----------------------')
 
 winner = False
+tie = False
 
 winning_combos = [
     ['a1', 'a2', 'a3'],
@@ -24,75 +25,88 @@ def init_game():
         'a3': None, 'b3': None, 'c3': None
     }
     turn = 'X'
-    # init_game(print(board['a1'], '|', board['a2'], '|', board['a3']))
-    # print('      A      B      C')
-    # print('')
-    # print('1)  ', board['a1'], '|', board['a2'], '|', board['a3'])
-    # print('    -------------------')
-    # print('2)  ', board['b1'], '|', board['b2'], '|', board['b3'])
-    # print('    -------------------')
-    # print('3)  ', board['c1'], '|', board['c2'], '|', board['c3'])
-    # print('    -------------------')
+
 
 init_game()
 
 def print_board():
-    print('      A      B      C')
+    print('     A     B     C')
     print('')
-    print('1)  ', board['a1'], '|', board['b1'], '|', board['c1'])
-    print('    -------------------')
-    print('2)  ', board['a2'], '|', board['b2'], '|', board['c2'])
-    print('    -------------------')
-    print('3)  ', board['a3'], '|', board['b3'], '|', board['c3'])
-    print('    -------------------')
+    print('1)  ', board['a1'] or ' ', ' | ', board['b1'] or ' ', ' | ', board['c1'] or ' ')
+    print('    ---------------')
+    print('2)  ', board['a2'] or ' ', ' | ', board['b2'] or ' ', ' | ', board['c2'] or ' ')
+    print('    ---------------')
+    print('3)  ', board['a3'] or ' ', ' | ', board['b3'] or ' ', ' | ', board['c3'] or ' ')
+    print('    ---------------')
 
 print_board()
 
+
 def get_winner():
     global winner
+    for combo in winning_combos:
+        if set(combo).issubset(set(move_x_combos)):
+            winner = True
+            print('Player X wins!')
+            init_game()
+            return
+        elif set(combo).issubset(set(move_o_combos)):
+            winner = True
+            print('Player O wins!')
+            init_game()
+            return
+    if len(move_x_combos) + len(move_o_combos) == 9:
+        winner = True
+        print("Another tie!")
+        init_game()
+
     if winner == True:
         init_game()
-    if move_x_combos in winning_combos:
-        winner = True
-        print('Player X wins!')
-    elif move_o_combos in winning_combos:
-        winner = True
-        print('Player O Wins!')
+
+
 
 def get_move():
-    global move_x, winner, move_x_combos, move_o_combos
+    global move_x, move_o, winner, move_x_combos, move_o_combos
     move_x_combos = []
     move_o_combos = []
+    turn = 'X'
+
     while winner == False:
-        move_x = input("Player X's turn: ")
-        if move_x in board:
-            board[move_x] = 'X'
-            print_board()
-            turn = 'O'
-            move_x_combos.append(move_x)
-            print(move_x_combos)
-            get_winner()
-        move_o = input("Player O's turn: ")
-        if move_o in board:
-            board[move_o] = 'O'
-            print_board()
-            move_o_combos.append(move_o)
-            turn = 'X'
-            print(move_o_combos)
-            get_winner()
-        # get_move()
-        print(move_x)
-        print(move_o)
+        while turn == 'X':
+            move_x = input("Player X's move (example B2): ").lower()
+
+            if move_x in board and board[move_x] == None:
+                board[move_x] = 'X'
+                print_board()
+                move_x_combos.append(move_x)
+                # print(move_x_combos)
+
+                get_winner()
+
+                if winner == True:
+                    break
+                turn = 'O'
+            else:
+                print('Bogus move! Try again...')
+                continue
+
+        while turn == 'O':
+            move_o = input("Player O's turn (example B2): ").lower()
+
+            if move_o in board and board[move_o] == None:
+                board[move_o] = 'O'
+                print_board()
+                move_o_combos.append(move_o)
+                # print(move_o_combos)
+
+                get_winner()
+
+                turn = 'X'
+            else:
+                print('Bogus move! Try again...')
+                continue
+
+        # print(move_x)
+        # print(move_o)
 
 get_move()
-
-# def get_winner():
-#     if move_x_combos in winning_combos:
-#         winner == True
-#         print('Player X wins!')
-
-# def get_winner():
-#     if move_x_combos in winning_combos:
-#         print('Player X wins!')
-
-# get_winner()
